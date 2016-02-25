c47b4430b55847aab02ba02db4057fe811a18446
Yann Cahuzac <YANN CAHUZAC/EXTERNE/SMABTP>
2016-02-25 16:20:55 +0100

    Gulp - 3 - dist - Erreur chargement module Angular

--- brico-war/gulpfile.js
+++ brico-war/gulpfile.js
@@ -18,17 +18,28 @@ var gulp = require('gulp'),
     replace = require('gulp-replace'),
 	htmlreplace = require('gulp-html-replace');
 
+/**
+ * Ne pas mettre les chemin absolus sinon gulp ne trouve plus les répertoires.
+ * Sauf pour le distJsAbsolu où sinon c'est le navigateur qui ne trouvera plus les *.min.js. 
+ */
 var paths = {
 	srcAppJs: 'src/main/webapp/script/js/**/*.js',
+	srcApp: 'src/main/webapp/script/js/**/',
+	srcAppSrv: 'src/main/webapp/script/js/services/',
+	srcAppCtrl: 'src/main/webapp/script/js/controllers/',
+	srcAppDir: 'src/main/webapp/script/js/directives/',
     srcUtilsJs: 'src/main/webapp/script/js/utils/*.js',
-    styles: 'src/main/webapp/script/css/**/*.*',
-    images: 'src/main/webapp/script/img/**/*.*',
+    srcUtils: 'src/main/webapp/script/js/utils/',
+    srcStyles: 'src/main/webapp/script/css/**/*.*',
+    srcImages: 'src/main/webapp/script/img/**/*.*',
     index: 'src/main/webapp/script/templates/index.html',
-    templates: 'src/main/webapp/script/templates/*.*',
-    bower_fonts: 'src/main/webapp/bower_components/**/*.{ttf,woff,eof,svg}',
+    srcTemplates: 'src/main/webapp/script/templates/*.*',
     dist: 'src/main/webapp/dist/',
-    app: 'src/main/webapp/script/',
+    distJs: 'src/main/webapp/dist/js/',
+    distJsAbsolu: '/brico-war/dist/js/',
     webapproot:'src/main/webapp',
+    appJsFinal: 'app.min.js',
+    utilsJsFinal: 'utils.min.js',
 };
 
 /**
@@ -39,37 +50,65 @@ gulp.task('clean', function(cb) {
 });
 
 /**
- * Handle bower components from index
- */
-gulp.task('usemin', function() {
-	return gulp
-		.src(paths.index)
-		.pipe(
-			usemin({
-				appJs : [ minifyJs(), 'concat', ngAnnotate(), uglify({outSourceMap : true}), rev() ],
-				vendorjs : [ rev() ]
-		}))
-		.pipe(gulp.dest(paths.dist));
-});
-
-/**
- * Minify tous les JS App pour les mettre dans index.min.js
+ * Minify tous les Js App pour les mettre dans paths.appJsFinal
+ * Respecter l'ordre des Js!
  */
 gulp.task('custom-appJs', function() {
-    return gulp.src(['!' + paths.srcUtilsJs, paths.srcAppJs])
+    return gulp.src(
+		[
+		 '!' + paths.srcUtilsJs, 
+		 paths.srcAppCtrl + 'app.js',
+		 
+		 paths.srcAppSrv + 'postSrv.js',
+		 paths.srcAppSrv + 'utilSrv.js',
+		 paths.srcAppSrv + 'themesSrv.js',
+		 paths.srcAppSrv + 'authSrv.js',
+		 paths.srcAppSrv + 'newAccountSrv.js',
+		 
+		 paths.srcAppCtrl + 'master-ctrl.js',
+		 paths.srcAppCtrl + 'homeCtrl.js',
+		 paths.srcAppCtrl + 'newPostCtrl.js',
+		 paths.srcAppCtrl + 'themeCtrl.js',
+		 paths.srcAppCtrl + 'postCtrl.js',
+		 paths.srcAppCtrl + 'newAccountCtrl.js',
+		 
+		 paths.srcAppDir + 'widget.js',
+		 paths.srcAppDir + 'widget-body.js',
+		 paths.srcAppDir + 'widget-footer.js',
+		 paths.srcAppDir + 'widget-header.js',
+		 paths.srcAppDir + 'loading.js',
+		 paths.srcAppDir + 'login.js',
+		 ]
+    	)
         .pipe(minifyJs())
-        .pipe(concat('index.min.js'))
-        .pipe(gulp.dest(paths.dist + 'js'));
+        .pipe(uglify())
+        .pipe(concat(paths.appJsFinal))
+        .pipe(gulp.dest(paths.distJs));
 });
 
 /**
- * Minify tous les JS Utils pour les mettre dans utils.min.js
+ * Minify tous les Js Utils pour les mettre dans paths.utilsJsFinal
+ * NB: les 2 select2.*js qu'on voit ci-dessous ne sont pas issus du même package (select2 et angular-ui-select2).
+ * NB: il faut indiquer l'ordre à gulp pour qu'il concat dans l'ordre souhaité.
  */
 gulp.task('custom-utilsJs', function() {
-	return gulp.src(paths.srcUtilsJs)
-	.pipe(minifyJs())
-	.pipe(concat('utils.min.js'))
-	.pipe(gulp.dest(paths.dist + 'js'));
+	return gulp.src(
+			[
+				paths.srcUtils + 'jquery.min.js',
+				paths.srcUtils + 'angular.min.js',
+				paths.srcUtils + 'bootstrap.min.js',
+				paths.srcUtils + 'ui-bootstrap-tpls.min.js',
+				paths.srcUtils + 'angular-cookies.min.js',
+				paths.srcUtils + 'angular-animate.min.js',
+				paths.srcUtils + 'angular-ui-router.min.js',
+				paths.srcUtils + 'select2.min.js',
+				paths.srcUtils + 'select2.js',
+				paths.srcUtils + 'angular-table.min.js'
+			]
+			)
+			// .pipe(minifyJs())
+			.pipe(concat(paths.utilsJsFinal))
+			.pipe(gulp.dest(paths.distJs));
 });
 
 /**
@@ -78,43 +117,36 @@ gulp.task('custom-utilsJs', function() {
 gulp.task('html-replace', function() {
 	  return gulp.src(paths.index)
 	    .pipe(htmlreplace({
-	        'appJs': paths.dist + 'js/index.min.js',
-	        'utilsJs': paths.dist + 'js/utils.min.js'
+	    	'utilsJs': paths.distJsAbsolu + paths.utilsJsFinal,
+	        'appJs': paths.distJsAbsolu + paths.appJsFinal
 	    }))
 	    .pipe(gulp.dest(paths.dist + 'templates'));
 });
 
-// ###########################################################
-// ###########################################################
-
-gulp.task('copy-bower_fonts', function() {
-    return gulp.src(paths.bower_fonts)
-        .pipe(rename({
-            dirname: '/fonts'
-        }))
-        .pipe(gulp.dest(paths.dist + 'lib'));
-});
+// ##########################################################
+// ########################## TODO ##########################
 
 gulp.task('custom-images', function() {
-    return gulp.src(paths.images)
+    return gulp.src(paths.srcImages)
         .pipe(gulp.dest(paths.dist + 'img'));
 });
 
-gulp.task('custom-less', function() {
-    return gulp.src(paths.styles)
-    	// Pb sur less(), voir les versions dans le package.json
-        // .pipe(less())
+gulp.task('custom-css', function() {
+    return gulp.src(paths.srcStyles)
         .pipe(gulp.dest(paths.dist + 'css'));
 });
 
+/**
+ * NB: on ne mets pas l'index.html car on le traite déjà ailleurs.
+ */
 gulp.task('custom-templates', function() {
-    return gulp.src(paths.templates)
+    return gulp.src([paths.srcTemplates, '!' + paths.index])
         .pipe(minifyHTML())
         .pipe(gulp.dest(paths.dist + 'templates'));
 });
 
-// ###########################################################
-// ###########################################################
+// ########################## FIN ##########################
+// #########################################################
 
 /**
  * Watch les modification sur chaque dossier décrit ci-dessous:
@@ -122,16 +154,13 @@ gulp.task('custom-templates', function() {
 gulp.task('watch', function() {
 	gulp.watch([paths.srcAppJs], ['custom-appJs', 'html-replace']);
 	gulp.watch([paths.srcUtilsJs], ['custom-utilsJs', 'html-replace']);
-
-	// A voir:
-	gulp.watch([paths.images], ['custom-images']);
-    gulp.watch([paths.styles], ['custom-less']);
-    gulp.watch([paths.templates], ['custom-templates']);
-    gulp.watch([paths.index], ['usemin']);
+	gulp.watch([paths.srcImages], ['custom-images']);
+    gulp.watch([paths.srcStyles], ['custom-css']);
+    gulp.watch([paths.srcTemplates], ['custom-templates']);
 });
 
 /**
- * Créé le proxy qui se fait passer pour un serveur static qui pointe vers les page HTML/CSS/JS.. 
+ * Créé le proxy qui se fait passer pour un serveur static qui pointe vers les page HTML/CSS/Js.. 
  */
 gulp.task('connect-dev', ['watch'], function() {
     connect.server({
@@ -161,11 +190,9 @@ function prismInit(prismMode) {
     });
 }
 
-gulp.task('default', ['webserver', 'livereload', 'watch']);
-gulp.task('build', ['usemin', 'html-replace', 'build-assets', 'build-custom']);
-
+gulp.task('buildStatic', ['custom-templates', 'custom-css', 'custom-images']);
 // Executer les tasks 'clean' puis 'buildBrico'
-gulp.task('buildBrico', ['custom-appJs', 'custom-utilsJs', 'html-replace']);
+gulp.task('build', ['custom-appJs', 'custom-utilsJs', 'html-replace', 'buildStatic']);
 
 gulp.task('proxy', ['connect-dev'], function() {
     prismInit('proxy');
